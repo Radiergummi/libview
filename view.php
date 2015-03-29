6f3e532d938968bb94bc80173ea6bf51da1d7360
Mo <Radiergummi@users.noreply.github.com>
2015-03-29 23:45:58 +0200

    Create view.php

---
+++ view.php
@@ -0,0 +1,18 @@
+<?
+// enable these if you intend to use this library in its own namespace!
+# namespace View;
+# 
+
+/**
+ * General purpose view class
+ * 
+ * @package libview
+ * @author Moritz Friedrich <m@9dev.de>
+ */
+class View {
+	/**
+	 * holds the current view
+	 * 
+	 * @var array
+	 */
+	private $view = array();
