15b53fe7d358fc9fd581f562bf60b5ac507dd25d
Mo <Radiergummi@users.noreply.github.com>
2015-04-01 12:23:05 +0200

    Added new features
    Custom template directories and setters, possibility to set default template

--- view.php
+++ view.php
@@ -26,11 +26,19 @@ class View
 
 
 	/**
-	 * the path to the current file (change this to whatever suits you)
+	 * the default path to the template directory, shared among all views
 	 * 
 	 * @var string
 	 */
-	const PATH = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+	public static $templateDir = '';
+
+
+	/**
+	 * the default (fallback) template to use, shared among all views
+	 * 
+	 * @var string
+	 */
+	public static $defaultTemplate = '';
 
 
 	/**
@@ -38,11 +46,43 @@ class View
 	 * 
 	 * @param string $template  the template file to work with
 	 * @param array $variables (optional)  the variables to replace in the template
+	 * @param string $templateDir (optional)  a custom template directory for this view
+	 * @param string $defaultTemplate (optional)  a fallback template in case the spicified isn't to be found
 	 */
-	public function __construct(string $template, array $variables = array())
+	public function __construct($template, array $variables = array(), $templateDir = '', $defaultTemplate = '')
 	{
 			$this->template = $template;
 			$this->variables = $variables;
+			static::$templateDir = (isset($templateDir))
+			    ? $templateDir
+			    : dirname(__FILE__) . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR
+			);
+			static::$defaultTemplate = (isset($defaultTemplate))
+			    ? $defaultTemplate
+			    : 'page'
+			);
+	}
+
+
+	/**
+	 * Sets a custom template directory
+	 * 
+	 * @param string $templateDir  a custom template directory for this view
+	 */
+	public function setTemplateDir($templateDir)
+	{
+		static::$templateDir = $templateDir;
+	}
+
+
+	/**
+	 * Sets a default template
+	 * 
+	 * @param string $defaultTemplate  a fallback template in case the spicified isn't to be found
+	 */
+	public function setdefaultTemplate($defaultTemplate)
+	{
+		static::$defaultTemplate = $defaultTemplate;
 	}
 
 
@@ -53,7 +93,7 @@ class View
 	 * @param string $template  the template file for the partial
 	 * @param array $variables (optional)  the template variables to use within the partial
 	 */
-	public function partial(string $name, string $template, array $variables = array())
+	public function partial($name, $template, array $variables = array())
 	{
 		$this->variables[$name] = (new View($template, $variables))->render();
 	}
@@ -65,7 +105,7 @@ class View
 	 * @param string $name  the variable name to use
 	 * @param mixed $value  the template file for the partial
 	 */
-	public function set(string $name, mixed $value)
+	public function set($name, $value)
 	{
 		$this->variables[$name] = $value;
 	}
@@ -94,7 +134,11 @@ class View
 		if (is_readable($theme_functions = PATH . 'theme_functions.php')) include $theme_functions;
 
 		// require the actual template
-		require PATH . 'templates/' . $this->template . '.php';
+		if (is_readable($this->template)) {
+		    require $this->templateDir . $this->template . '.php';
+		} else {
+		    require $this->templateDir . static::$defaultTemplate . '.php';
+		}
 
 		// returb the collected output
 		return ob_get_clean();
