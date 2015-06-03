a066105c5d990427cdec0e5f67be0c0b152664b0
Mo <Radiergummi@users.noreply.github.com>
2015-06-03 20:13:27 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,4 @@
  * 
  */
 define('DS', DIRECTORY_SEPARATOR);
-require dirname(__FILE__) . DS . '..' . DS  . 'src' . DS . 'libview.php';
+require dirname(__FILE__) . DS . '..' . DS  . 'src' . DS . 'view.php';
